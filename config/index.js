f4b43b426e06f3aa34113d961fb24a0aca6da3b5
Bin Le <binle@BL-DDA-MBP.local>
2020-03-08 16:48:11 -0400

    deploy to docs folder

--- config/index.js
+++ config/index.js
@@ -45,11 +45,17 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    // index: path.resolve(__dirname, '../dist/index.html'),
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    // assetsRoot: path.resolve(__dirname, '../dist'),
+    // assetsSubDirectory: 'static',
+    // assetsPublicPath: '/',
+
+    // deployment to docs folder as alternative to gh-pages branch
+    index: path.resolve(__dirname, '../docs/index.html'),
+    assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '',
 
     /**
      * Source Maps
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,8 +1,5 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"',
+  NODE_ENV: '"production"'
 
-  // this is for github gh-page deployment
-  // @see deploy.sh
-  assetsPublicPath: '/example-todo-vue'
 }
